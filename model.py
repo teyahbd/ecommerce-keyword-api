1e10de9ef5f81ac68cc4d64576afe517b33ec4c4
teyahbd <teyahbrennendavies@gmail.com>
2022-11-23 13:12:45 +0000

    add cleaner ecommerce word vectors

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from gensim.models import KeyedVectors
 
-model = KeyedVectors.load_word2vec_format('ecommerce_vectors.txt', binary=False) 
+model = KeyedVectors.load_word2vec_format('new_ecommerce_vecs.txt', binary=False) 
 
 def findKeywords(likedKeywords, dislikedKeywords):
     result = model.most_similar(positive=likedKeywords, negative=dislikedKeywords, topn=2)
